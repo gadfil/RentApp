ecec90324028a78c8aaaa1f9123307159dd7023d
Elena <elena.gamega@gmail.com>
2018-01-30 21:23:19 +0200

    added icons and cookies

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
+
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
  * @flow
  */
 import React, {Component} from 'react';
@@ -12,35 +11,43 @@ import {
     View,
     Button,
     NetInfo,
-    Linking,
+    BackAndroid,
+    ActivityIndicator,
+    BackHandler,
     Platform,
     StatusBar,
     Dimensions,
-    TouchableOpacity
+    TouchableOpacity,
+    Keyboard,
+    AsyncStorage,
 } from 'react-native';
-import Orientation from 'react-native-orientation';
+// import Orientation from 'react-native-orientation';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
 import CookieManager from 'react-native-cookies';
-
-const LOGIN_URL = "https://my.serverscheck.com/";
-const HOME_URL = "https://my.serverscheck.com/home.php";
 
 const dimen = Dimensions.get('window');
 const isIphoneX = () => Platform.OS === 'ios' &&
     !Platform.isPad &&
     !Platform.isTVOS &&
     (dimen.height === 812 || dimen.width === 812)
+
+const marginTop = ()=>Platform.OS ==='ios'?isIphoneX()?45:25:0
+
 const styles = StyleSheet.create({
     container: {
         flex: 1, backgroundColor: 'rgb(254, 143, 29)'
     },
 
+    webView: {
+        flex: 1,
+        marginTop:marginTop()
+    },
     instructions: {
         textAlign: 'center',
         fontSize: 17,
         color: 'rgb(254, 143, 29)',
         marginBottom: 5,
-        marginTop:dimen.height*0.3
+        marginTop: dimen.height * 0.3
     },
     btnClickContain: {
         flex: 1,
@@ -49,10 +56,6 @@ const styles = StyleSheet.create({
         alignItems: 'stretch',
         alignSelf: 'stretch',
         backgroundColor: 'rgb(254, 143, 29)',
-        // borderRadius: 5,
-        // padding: 5,
-        // marginTop: 5,
-        // marginBottom: 5,
     },
     btnContainer: {
         flex: 1,
@@ -60,7 +63,6 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         alignSelf: 'center',
-        // borderRadius: 2,
     },
     btnIcon: {
         height: 25,
@@ -69,6 +71,9 @@ const styles = StyleSheet.create({
 });
 
 export default class App extends Component<{}> {
+    renderLoading = () => <ActivityIndicator size="large" style={{marginTop:dimen.height*0.5}}/>;
+    renderLoadingWithData =()=><ActivityIndicator size="large" style={{height:dimen.height, marginTop:0.5}}/>
+
     constructor(props) {
         super(props);
         this.state = {
@@ -77,46 +82,60 @@ export default class App extends Component<{}> {
             canGoForward: false,
             loggedIn: false,
             loadedCookie: false,
+            canGoHome: false,
+            marginTop: 45,
+            login: null,
+            password: null,
+            showPreloader: false,
+            url: ""
         }
+
+        this._onNavigationStateChange = this._onNavigationStateChange.bind(this);
+        this._onMessage = this._onMessage.bind(this);
+        this._onLoDEnd = this._onLoDEnd.bind(this);
+    }
+
+    componentWillMount () {
+        this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this._keyboardDidShow.bind(this));
+        this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this._keyboardDidHide.bind(this));
 
     }
 
+    componentWillUnmount () {
+        this.keyboardDidShowListener.remove();
+        this.keyboardDidHideListener.remove();
+        BackHandler.removeEventListener('hardwareBackPress', this._backButton);
+    }
+
+    _backButton(){
+        console.log('_back', this.state)
+        if (this.state.canGoBack) {
+            this._bridge.goBack()
+            return true;
+        }
+        return false;
+    }
+    _keyboardDidShow () {
+        this.setState({isKeyboardShow:true})
+    }
+
+    _keyboardDidHide () {
+        this.setState({isKeyboardShow:false})
+    }
     componentDidMount() {
         NetInfo.isConnected.fetch().done(isConnected => this.setState({isConnected}))
         // this.checkNewVersion()
-        Orientation.addOrientationListener(this._orientationDidChange);
-
-        CookieManager.get(HOME_URL).then((res) => {
-            let isAuthenticated;
-            if (res && res.hasOwnProperty('__cfduid')) {
-                isAuthenticated = true;
-                let coocies = ''
-                for(let key in res){
-                    coocies+=`${key}=${res[key]}; `
-                }
-                this.setState({
-                    cookie: coocies
-                })
-                console.log('coocies', coocies)
-            }
-            else {
-                isAuthenticated = false;
-            }
-            console.log('CookieManager.get =>', res); // => 'user_session=abcdefg; path=/;'
-            this.setState({
-                loggedIn: isAuthenticated,
-                loadedCookie: true
+        // Orientation.addOrientationListener(this._orientationDidChange);
+        BackHandler.addEventListener('hardwareBackPress', this._backButton.bind(this));
+        console.log('cookie', window.document, window.document?window.document.cookie:'');
+        CookieManager.get('https://my.serverscheck.com')
+            .then((res) => {
+                console.log('CookieManager.get =>', res); // => 'user_session=abcdefg; path=/;'
             });
-        });
-    }
-
-    _orientationDidChange = (orientation) => {
-        this._bridge.forceUpdate()
-        if (orientation === 'LANDSCAPE') {
-            // do something with landscape layout
-        } else {
-            // do something with portrait layout
-        }
+        // this._bridge.execute('alert("hello world")')
+        setTimeout(()=>this.setState({login:null, password:null}), 5000)
+        this._bridge.postMessage("r57ytyiuruytiurtyiu")
+        console.log('bridge', this._bridge)
     }
 
 
@@ -136,93 +155,178 @@ export default class App extends Component<{}> {
         )
     }
 
-    _onNavigationStateChange(navState) {
-        console.log('navState',navState, this._bridge)
-        if (navState.url == HOME_URL) {
+    _onNavigationStateChange=(navState)=> {
+        // console.log('navState', navState, this._bridge)
+        if (navState.url.search('https://my.serverscheck.com/home.php') === 0||navState.url.search('https://my.serverscheck.com/index.php') === 0 ) {
             this.setState({
                 loggedIn: true,
+                canGoHome: false
+
+            });
+        } else {
+            this.setState({
+                canGoHome: navState.canGoBack,
             });
         }
+        if (navState.url.search('https://my.serverscheck.com/logout.php') === 0){
+                console.log('logout')
+            this._bridge.injectJavaScript("localStorage.clear();")
+        }
         this.setState({
+            url: navState.url,
             canGoBack: navState.canGoBack,
             canGoForward: navState.canGoForward,
         });
-    }
+    };
+
+    _onLoDEnd = (data)=>{
+        if (this.state.url.search('https://my.serverscheck.com/home.php')===0){
+            this.setState({showPreloader:false});
+        }else if(this.state.url.search('https://my.serverscheck.com/login.php')===0||this.state.url==='https://my.serverscheck.com/'){
+            console.log('onLoadEnd', data);
+            this.setState({showPreloader:true});
+        }
+        console.log('onLoadEnd', this.state.showPreloader, this.state.url, this.state.url.search('https://my.serverscheck.com/login.php')===0&&this.state.login!==null&&this.state.password!==null);
+    };
+
+    _onMessage=(event)=> {
+        //Prints out data that was passed.
+        // let value = JSON.parse(event)
+        let data =[];
+        if (event.nativeEvent.data&&event.nativeEvent.data.length!==0&&event.nativeEvent.data!=="null,null"){
+            data = event.nativeEvent.data.split(",");
+            this.setState({login:data[0], password:data[1]});
+        }else{
+            this.setState({login:null, password:null, showPreloader:false})
+        }
+
+        console.log("onMessage",event.nativeEvent.data);
+    };
     render() {
-
-        console.log('isConnected', this.state.isConnected)
-        // marginTop: Platform.OS == 'ios' ? 20 : 0        if (!this.state.isConnected) {
-
-        // if (!this.state.isConnected) {
-        //     this.renderError()
-        // }
-        // let source = this.state.cookie?{}
+        let jsCode = `
+        
+        document.querySelectorAll("body > div > form > button")[0].addEventListener('click', function(event) {
+            var login = document.getElementById("inputEmail").value;
+            var password = document.getElementById("inputPassword").value;
+           
+            localStorage.setItem("login", login);
+            localStorage.setItem("password", password);            
+        });   
+        let log = localStorage.getItem("login");
+        let pass =  localStorage.getItem("password");
+        setInterval(window.postMessage(log+","+pass),1000);
+        
+        if (
+        window.location.href === "https://my.serverscheck.com/"
+        && log!==undefined
+        && log.length!==0
+        && pass!==undefined
+        && pass.length!==0
+        ){
+           document.getElementById("inputEmail").value=log;
+           document.getElementById("inputPassword").value=pass;
+           var login=document.getElementById("inputEmail").value;
+           var password=document.getElementById("inputEmail").value;
+           
+           if (login.length!==0&&password.length!==0){
+            document.querySelectorAll("body > div > form > button")[0].click();
+           }                    
+            
+        }
+        if (window.location.href.search("https://my.serverscheck.com/home.php")===0){
+        alert("ghgg");
+        }
+        document.querySelectorAll("body > div > div.hidden-lg.hidden-md.hidden-sm.col-xs-12 > div > div > div > div > div.col-xs-10.text-left > a")[0].addEventListener('click', function(event) {
+            var login = document.getElementById("inputEmail").value;
+            var password = document.getElementById("inputPassword").value;
+           alert("logout");
+            // localStorage.setItem("login", login);
+            // localStorage.setItem("password", password);            
+        });   
+    `;
+        // const jsCode = "window.postMessage(document.cookie)";
+        console.log('cookie', window&&window.document?window.document.cookie:'')
+        // console.log('state', this.state)
         return (
             <View style={{
                 flex: 1, backgroundColor: 'rgb(254, 143, 29)'
             }}>
+                {this.state.showPreloader===true
+                        ?
+                            this.renderLoadingWithData()
+                                :
+                                null
+                }
                 <StatusBar
                     barStyle="light-content"
+                    backgroundColor="#c56000"
                 />
                 <WebView
                     mixedContentMode='always'
                     source={{
-                        uri: 'https://my.serverscheck.com/home.php',
-                        // headers:{cookie: "AWSELB=9173D589162CFBC8D07BAF076CEBC9CD2D733A268C106E3B0E721C49F871540A553A308563DE1EAB0482C0811F6A43AB65178957BFA0C17382744682476B76AE86542310C0; PHPSESSID=7mjve4nvvhie3vjvrm0pt0lolb; __cfduid=da9d4b497ca537bdb86504db2f8e593e81516987633"}
-                        headers:{cookie: ".2.1566498311.1516885310; _gid=GA1.2.826278415.1516885310; PHPSESSID=rk9qde7j55d1acf1s2i7mmu40t; AWSELB=9173D589162CFBC8D07BAF076CEBC9CD2D733A268C106E3B0E721C49F871540A553A308563DE1EAB0482C0811F6A43AB65178957BFA0C17382744682476B76AE86542310C0"}
+                        // uri: 'https://github.com/',
+                        uri: 'https://my.serverscheck.com/',
                     }}
+                    javaScriptEnabled={true}
                     ref={(b) => this._bridge = b}
-                    style={{flex: 1, marginTop: 45}}
+                    style={styles.webView}//568 iPhone 5s 667 iPhone 6s
                     onError={() => NetInfo.isConnected.fetch().done(isConnected => this.setState({isConnected}))}
                     renderError={() => this.renderError()}
-                    onNavigationStateChange={this._onNavigationStateChange.bind(this)}
+                    onNavigationStateChange={this._onNavigationStateChange}
+                    onLoadEnd={this._onLoDEnd}
+                    injectedJavaScript={jsCode}
+                    onShouldStartLoadWithRequest={this.renderLoading}
+                    onMessage={(data)=>this._onMessage(data)}
+                    startInLoadingState={true}
+                    renderLoading={this.renderLoading}
+
+
                 />
-                <View style={{height:70, backgroundColor:'rgb(254, 143, 29)', flexDirection:'row'}}>
+                <View style={{height: this.state.isKeyboardShow?0:isIphoneX()?70:50, backgroundColor: 'rgb(254, 143, 29)', flexDirection: 'row'}}>
                     <TouchableOpacity
-                        onPress={()=> this._bridge.goBack()}
+                        onPress={() => this._bridge.goBack()}
                         style={styles.btnClickContain}
-                        disabled ={!this.state.canGoBack}
+                        disabled={!this.state.canGoBack}
                     >
                         <View
                             style={styles.btnContainer}>
                             <Icon
                                 name="angle-left"
-                                size={30}
-                                color={!this.state.canGoBack?"#ffffff":"#0000ff"}
+                                size={25}
+                                color={!this.state.canGoBack ? "#ffffff" : "#A52A2A"}
                                 style={styles.btnIcon}/>
                         </View>
                     </TouchableOpacity>
                     <TouchableOpacity
-                        onPress={()=> this._bridge.startInLoadingState}
+                        onPress={() => {
+                            this._bridge.injectJavaScript("window.location.href='https://my.serverscheck.com/home.php'");
+                        }}
                         style={styles.btnClickContain}
-                        // disabled ={!this.state.canGoHome}
+                        disabled={!this.state.canGoHome}
                     >
                         <View
                             style={styles.btnContainer}>
                             <Icon
                                 name="home"
-                                size={30}
-                                color={!this.state.canGoBack?"#ffffff":"#0000ff"}
+                                size={25}
+                                color={!this.state.canGoHome ? "#ffffff" : "#A52A2A"}
                                 style={styles.btnIcon}/>
                         </View>
                     </TouchableOpacity>
                     <TouchableOpacity
-                        onPress={()=> this._bridge.goForward()}
+                        onPress={() => this._bridge.goForward()}
                         style={styles.btnClickContain}
-                        disabled ={!this.state.canGoForward}
+                        disabled={!this.state.canGoForward}
                     >
                         <View
                             style={styles.btnContainer}>
                             <Icon
                                 name="angle-right"
-                                size={30}
-                                color={!this.state.canGoForward?"#ffffff":"#0000ff"}
+                                size={25}
+                                color={!this.state.canGoForward ? "#ffffff" : "#A52A2A"}
                                 style={styles.btnIcon}/>
                         </View>
                     </TouchableOpacity>
-
-                    {/*<Button  disabled ={!this.state.canGoBack} title="Back" onPress={()=> this._bridge.goBack()}/>*/}
-                    {/*<Button disabled ={!this.state.canGoForward} title="Forward"  onPress={()=> this._bridge.goForward()}/>*/}
                 </View>
             </View>
         );
